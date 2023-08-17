b23107093f4bc87e3a3bbc7268e18951290a1ac2
NEETNETNET <112066731+NEETNETNET@users.noreply.github.com>
2023-08-17 21:44:36 +0700

    java

---
+++ CHELLO - Hello World.java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello PTIT.");
+    }
+    
+}
